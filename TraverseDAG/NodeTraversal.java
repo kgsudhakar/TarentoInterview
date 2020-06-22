642ce7fff7ab5cad4cce02c79389e0d59308a6a2
kgsudhakar <sudhakargovind@gmail.com>
2020-06-22 13:38:59 +0530

    Added comments

--- TraverseDAG/NodeTraversal.java
+++ TraverseDAG/NodeTraversal.java
@@ -3,7 +3,19 @@ package TraverseDAG;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class traverses from the top node to bottom to print all possible path.
+ */
 public class NodeTraversal {
+    
+    /**
+     * Recursive method to obtain and print all possible path traversing from the given node.
+     * When each node is processed, path to that node from the top is passed as input.
+     * The printed path will be like 1->2->3
+     *
+     * @param node - Node to start traversal.
+     * @param currentPath - This represents the path to the given node from the top node.
+     */    
     public static void walk(Node node, String currentPath) {
         List<Node> children = node.getChildren();
         for (Node childNode : children) {
